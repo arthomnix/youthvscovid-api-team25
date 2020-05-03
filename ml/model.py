16044091b80f0fc41dfeca382359017fa8a3f9fa
arthomnix <35371030+arthomnix@users.noreply.github.com>
2020-05-03 19:36:14 +0100

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -1,3 +1,21 @@
+# Most of this code is from https://www.datatechnotes.com/2019/06/text-classification-example-with-keras.html, modified by me.
+# I give most of the credit for this to whoever wrote that tutorial. 
+#
+#Team 25 YouthVSCovid API - model.py - machine learning model for non-covid news
+#Copyright (C) 2020 arthomnix
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published
+#by the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU Affero General Public License for more details.
+#
+#You should have received a copy of the GNU Affero General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential, load_model
