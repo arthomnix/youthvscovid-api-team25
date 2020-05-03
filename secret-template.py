c318c392009596c0d1b74602de39a36dc8c5d2c3
Thomas Roberts <arthomnix@arthomnix.dev>
2020-05-03 19:05:45 +0100

    add secret-template.py

---
+++ secret-template.py
@@ -0,0 +1,3 @@
+# File for API keys - copy to 'secret.py' when you put valid keys in.
+apiKey = "API KEY REDACTED" # replace this with a Google Cloud API key with Google Places API enabled
+newsApiKey = "API KEY REDACTED" # replace this with a NewsAPI.org API key
